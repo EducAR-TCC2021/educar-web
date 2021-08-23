0c58d320fccbb04f20a70ed6bd27de21d3d095e6
Tiago <riverotiago@gmail.com>
2021-08-23 16:48:10 -0300

    CRLF -> LF

--- src/pages/editor/Editor.jsx
+++ src/pages/editor/Editor.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import TopMenu from "../../components/TopMenu";
+import Box from '@material-ui/core/Box';
+import TopMenu from '../../components/TopMenu';
 import SideMenu from './SideMenu';
-
 
 export default function Editor() {
   return (
     <div>
       <TopMenu />
       <SideMenu>
-
+        <Box> Cool </Box>
       </SideMenu>
       <EditorViewport />
       <SideMenu>
-
+        <Box> Other Cool </Box>
       </SideMenu>
     </div>
   );
---
+++ src/pages/editor/EditorViewport.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Box from '@material-ui/core/Box';
+
+export default function EditorViewport() {
+  return (
+    <Box> Viewport </Box>
+  );
+}
