0d2e3493e7af9a42e67208388948e1523727779d
Tiago <riverotiago@gmail.com>
2021-08-25 18:01:01 -0300

    Adicionados botões de adicionar e remover

--- src/pages/editor/OverlayMenu.jsx
+++ src/pages/editor/OverlayMenu.jsx
@@ -1,9 +1,32 @@
+import { Box, IconButton, makeStyles } from '@material-ui/core';
+import { Add, Remove } from '@material-ui/icons';
 import React from 'react';
 import SideSubMenu from './SideSubMenu';
 
+const useStyles = makeStyles(() => ({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+}));
+
 function OverlayMenu() {
+  const classes = useStyles();
   return (
-    <SideSubMenu title="Overlays" />
+    <SideSubMenu
+      title="Overlays"
+      options={(
+        <Box className={classes.container}>
+          <IconButton size="small">
+            <Add />
+          </IconButton>
+          <IconButton size="small">
+            <Remove />
+          </IconButton>
+        </Box>
+      )}
+    />
   );
 }
 
