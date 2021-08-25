8da61d48d6ee0332962f17d2b279bef7d871aa2b
Tiago <riverotiago@gmail.com>
2021-08-24 22:49:53 -0300

    Layout esqueleto organizado, Adicionada barra de ferramentas e sub-menus laterais

--- src/pages/editor/Editor.jsx
+++ src/pages/editor/Editor.jsx
@@ -1,14 +1,46 @@
-import React from 'react';
 import { Box, makeStyles } from '@material-ui/core';
+import { Cached, Height, OpenWith } from '@material-ui/icons';
+import React from 'react';
+import NextPageButton from '../../components/NextPageButton';
+import ProfileDropdown from '../../components/ProfileDropdown';
 import TopMenu from '../../components/TopMenu';
-import SideMenu from './SideMenu';
 import EditorViewport from './EditorViewport';
+import IconTool from './IconTool';
+import SceneTextField from './SceneTextField';
+import SideMenu from './SideMenu';
+import SideSubMenu from './SideSubMenu';
 
 const useStyles = makeStyles(() => ({
   horizontal: {
     display: 'flex',
     flexDirection: 'row',
-    height: 'calc(100vh - 48px )',
+    height: 'calc(100vh - 48px)',
+  },
+  toolbar: {
+    flex: 1000,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  toolbarLeft: {
+    flex: 1,
+    display: 'flex',
+    marginRight: 'auto',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  toolbarCenter: {
+    flex: 1,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  toolbarRight: {
+    flex: 1,
+    display: 'flex',
+    marginLeft: 'auto',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
   },
 }));
 
@@ -16,14 +48,30 @@ export default function Editor() {
   const classes = useStyles();
   return (
     <div>
-      <TopMenu />
+      <TopMenu hideLogo>
+        <div className={classes.toolbar}>
+          <div className={classes.toolbarLeft}>
+            <IconTool toolName="" icon={<OpenWith />} />
+            <IconTool toolName="" icon={<Cached />} />
+            <IconTool toolName="" icon={<Height />} />
+          </div>
+          <div className={classes.toolbarCenter}>
+            <SceneTextField />
+          </div>
+          <div className={classes.toolbarRight}>
+            <ProfileDropdown />
+            <NextPageButton>Salvar Cena</NextPageButton>
+          </div>
+        </div>
+      </TopMenu>
       <Box className={classes.horizontal}>
         <SideMenu>
-          <Box> Cool </Box>
+          <SideSubMenu title="Overlays" />
+          <SideSubMenu title="Adicionar um Link" />
         </SideMenu>
         <EditorViewport />
         <SideMenu>
-          <Box> Other Cool </Box>
+          <SideSubMenu title="Info" />
         </SideMenu>
       </Box>
     </div>
