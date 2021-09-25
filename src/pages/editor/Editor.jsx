ff76d1e2eefda5986ab9d84430ba4414c6ca7df1
riverotiago <riverotiago@gmail.com>
2021-09-25 20:38:14 -0300

    Adicionado logo na barra superior do editor

--- src/pages/editor/Editor.jsx
+++ src/pages/editor/Editor.jsx
@@ -13,6 +13,7 @@ import SceneTextField from './SceneTextField';
 import SideMenu from './SideMenu';
 import SideSubMenu from './SideSubMenu';
 import SaveSceneButton from './SaveSceneButton';
+import Logo from '../../components/Logo';
 
 const useStyles = makeStyles(() => ({
   horizontal: {
@@ -58,6 +59,7 @@ export default function Editor() {
       <TopMenu hideLogo>
         <div className={classes.toolbar}>
           <div className={classes.toolbarLeft}>
+            <Logo />
             <IconTool
               toolName="Translação"
               icon={<OpenWith />}
