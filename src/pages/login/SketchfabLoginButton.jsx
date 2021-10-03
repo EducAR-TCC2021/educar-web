309c6b8c24cecfa3a95e76e4214c2f0e136b2455
riverotiago <riverotiago@gmail.com>
2021-10-03 16:43:05 -0300

    update base_url

--- src/pages/login/SketchfabLoginButton.jsx
+++ src/pages/login/SketchfabLoginButton.jsx
@@ -11,7 +11,7 @@ import { accountActions } from '../../state/slices/account';
 
 const CLIENT_ID = 'Qj6hQl5K04dccP4SsGykPq4Pyp8kTYkny5gAqBBY';
 const SKETCHFAB_URL = `https://sketchfab.com/oauth2/authorize/?response_type=token&client_id=${CLIENT_ID}&approval_prompt=auto`;
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'https://educar-web.netlify.app';
 
 const useStyles = makeStyles((theme) => ({
   submit: {
