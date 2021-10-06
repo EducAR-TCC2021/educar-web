8f0758005499b63c696dea11e35377e2bd35e93c
riverotiago <riverotiago@gmail.com>
2021-10-05 22:45:19 -0300

    Revert "Deletar canal"
    This reverts commit 748189dcdae561b995d54af934aa3ec8456f5b5a.

--- src/state/requests/channel.js
+++ src/state/requests/channel.js
@@ -60,24 +60,10 @@ const deleteChannel = ({
   },
 });
 
-const deleteScene = ({
-  accessToken,
-  channelId,
-  sceneId,
-}) => ({
-  method: 'delete',
-  url: `/channels/${channelId}/scenes/${sceneId}`,
-  baseURL: BASE_URL,
-  headers: {
-    authorizationToken: accessToken,
-  },
-});
-
 const channelRequests = {
   updateScene,
   createChannel,
   deleteChannel,
-  deleteScene,
 };
 
 export default channelRequests;
